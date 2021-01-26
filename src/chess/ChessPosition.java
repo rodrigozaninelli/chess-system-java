8d96b36faa872cc340355c9ab517dc671c5ca8bd
Rodrigo Zaninelli <rodrigo.zaninelli@edu.unifil.br>
2021-01-26 17:34:44 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,44 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8){
+            throw new ChessException("Error instantiating ChessPosition. Vlaid values are from a1 to h8.");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    //matrix_row = 8 - chess_row
+    //'a'-'a' = 0
+    //'b'-'a' = 1
+    //'c'-'a' = 2
+    //matris_column = chess_column - 'a'
+
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
